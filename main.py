3f32bbf79b9eefe68c51088e3db388e4257e7e68
Sanjitha-A <angaru.sanjitha@gmail.com>
2025-11-04 17:48:33 +0000

    finished level 1 and level 2, working on level 3 right now.

--- main.py
+++ main.py
@@ -2,19 +2,50 @@ from library_books import library_books
 from datetime import datetime, timedelta
 
 # -------- Level 1 --------
-# TODO: Create a function to view all books that are currently available
+# TODO: Create a function to view all books that are currently available      (COMPLETED)
 # Output should include book ID, title, and author
+
+def available():
+    #for the for loop, I asked on AI to understand how to print the dictionary.
+    for book in library_books:
+        print(f'ID: {book['id']}')
+        print(f'Title: {book['title']}')
+        print(f'Author: {book['author']}')
+        print('') #to separate the books.
+available()
+
+    
+
 
 
 # -------- Level 2 --------
-# TODO: Create a function to search books by author OR genre
+# TODO: Create a function to search books by author OR genre                  (COMPLETED)
 # Search should be case-insensitive
 # Return a list of matching books
+
+def search_list():
+    search = input("What would you like to search for? enter a genre or author: ").title()
+    for book in library_books:
+        #in the if condition I checked is in the book for loop, I understood why we need to for loop in this by looking it up.
+        if book['author'].title() == search or book['title'].title() == search:
+            print(f"Here are some results that we found matching your input: \n Author: {book['author']} and Title: {book['title'] }")
+    if book['author'].title() != search or book['title'].title() != search:
+        print("This title or author is not in our library.")
+search_list()
+        
+
 
 
 # -------- Level 3 --------
 # TODO: Create a function to checkout a book by ID
 # If the book is available:
+for book in library_books:
+    id_search = input("Please enter the book ID: ")
+    if id_search == book['id']:
+        print(book['id'])
+    if id_search != book['id']:
+        print("Not a valid ID.")
+    
 #   - Mark it unavailable
 #   - Set the due_date to 2 weeks from today
 #   - Increment the checkouts counter
